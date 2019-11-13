b0f76c6ca04733ee08b64b2225ce8b46c0a53f7e
Elias Bernstein <41696293+ebernstein00@users.noreply.github.com>
2019-11-13 15:18:25 -0500

    Update stat.c

--- stat.c
+++ stat.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <time.h>
 
+struct stat buffer;
+int status;
+
+int main(){
+  status = stat("/dev/random", &buffer);
+  if (status < 0){
+    printf("Error #%d: %s\n", errno, strerror(errno));
+  }
+  printf("Information for selected file:\n");
+  printf("File Size:\t\t%d bytes\n", buffer.st_size);
+  printf("File mode:\t\t%o\n", buffer.st_mode);
+  printf("Last access time:\t\t%s\n", ctime(&buffer.atime));
+}
